715f16979549ecdadc58db718ca12f0ff7e3a36a
chiranthhc <12chiranth@gmail.com>
2023-02-04 13:16:11 +0530

    Transaction-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import Header from "./components/Header";
-import './App.css'
+import "./App.css";
 import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 const App = () => {
   return (
-  <>
-    <div>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-      </div>
-    </div>
-  </>
+    <>
+      <GlobalProvider>
+        <div>
+          <Header />
+          <div className="container">
+            <Balance />
+            <IncomeExpenses />
+            <TransactionList />
+            <AddTransaction />
+          </div>
+        </div>
+      </GlobalProvider>
+    </>
   );
 };
 
